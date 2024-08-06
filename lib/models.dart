dbadd6ba3c7094153e6ed3a04ac75373157f3706
SudenazYazici <sudenyazici@gmail.com>
2024-08-06 11:48:56 +0300

    ticket model moved from profile to models file

--- lib/models.dart
+++ lib/models.dart
@@ -112,3 +112,28 @@ class Seat {
     );
   }
 }
+
+class Ticket {
+  final int id;
+  final int cinemaId;
+  final String movieName;
+  final DateTime date;
+  final int price;
+
+  Ticket(
+      {required this.id,
+      required this.cinemaId,
+      required this.movieName,
+      required this.date,
+      required this.price});
+
+  factory Ticket.fromJson(Map<String, dynamic> json) {
+    return Ticket(
+      id: json['id'],
+      cinemaId: json['cinemaId'],
+      movieName: json['movieName'],
+      date: DateTime.parse(json['date']),
+      price: json['price'],
+    );
+  }
+}
--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -4,30 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class Ticket {
-  final int id;
-  final int cinemaId;
-  final String movieName;
-  final DateTime date;
-  final int price;
-
-  Ticket(
-      {required this.id,
-      required this.cinemaId,
-      required this.movieName,
-      required this.date,
-      required this.price});
-
-  factory Ticket.fromJson(Map<String, dynamic> json) {
-    return Ticket(
-      id: json['id'],
-      cinemaId: json['cinemaId'],
-      movieName: json['movieName'],
-      date: DateTime.parse(json['date']),
-      price: json['price'],
-    );
-  }
-}
+import 'models.dart';
 
 Future<List<Ticket>> fetchTickets(int userId) async {
   final response = await http
